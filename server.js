24e2faf0c2fa4f6c244cd21ac6a4886f2d3636d8
Carlos Rojas <caranroja@outlook.com>
2022-09-24 20:03:07 -0500

    Se realiza el cambio de puerto de la app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const {mongoConnection} = require('./database/configuration');
 require('dotenv').config();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 app.listen(app.get('port'), () => {
     console.log(`Escuchando en puerto ${app.get('port')}`);
